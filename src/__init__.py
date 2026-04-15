c84e73abe59fe6f1c723f1d5844dead6a018cd7e
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-04-15 13:32:53 +0500

    structure corrected

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+# Social Media AI Employee - Source Package
+from .analytics_tracker import AnalyticsTracker, increment_emails_sent, increment_whatsapp_sent, increment_ai_posts_generated, get_analytics
+from .utils import load_env, send_test_message, create_sample_config
